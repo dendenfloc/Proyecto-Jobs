b2941bbdf9533cd93b689e0cc97bb4a93d6e98ce
brunocse123 <bruno.sanchez@myper.com.pe>
2022-07-26 23:08:47 -0500

    mis cambios wen

---
+++ Jobs/Jobs/Rama wenda/Class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jobs.Rama_wenda
+{
+    public class Class
+    {
+        public string asdasd {get;set;}
+
+    }
+}
